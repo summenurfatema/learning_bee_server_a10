5bf1f7e8e0002a757c25dfbbdbdd4a02d75a05af
Fatema04 <shadrinmoni15@gmail.com>
2022-10-24 08:39:48 -0700

    COURSE details ADDED

---
+++ data/course-details.js
@@ -0,0 +1,152 @@
+[
+    {
+        "id": "012",
+
+        "course_id": "01",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Md Mahin",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    },
+    {
+        "id": "013",
+
+        "course_id": "02",
+        "rating": {
+            "number": 4.6,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with CSS",
+        "mentor": {
+            "name": "Md Tushar",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    },
+    {
+        "id": "014",
+
+        "course_id": "03",
+        "rating": {
+            "number": 4.6,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Ayesha Siddik",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    },
+    {
+        "id": "015",
+
+        "course_id": "04",
+        "rating": {
+            "number": 4.7,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Md Saifuddin",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    },
+    {
+        "id": "016",
+
+        "course_id": "05",
+        "rating": {
+            "number": 4.7,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Md Saifuddin",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    },
+    {
+        "id": "017",
+
+        "course_id": "06",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Miftaul Jannat",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    }, {
+        "id": "018",
+
+        "course_id": "07",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Bablu Sarkar",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    }, {
+        "id": "019",
+
+        "course_id": "08",
+        "rating": {
+            "number": 4.5,
+            "badge": "Excellent"
+        },
+
+        "title": "Let's start with HTML",
+        "mentor": {
+            "name": "Jannatul Mawa",
+            "class_duration": "1.30 mins",
+            "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+        "thumbnail_url": "https://i.ibb.co/QnwC4sG/unsplash-Eh-Tc-C9s-YXsw-11.png",
+        "image_url": "https://i.ibb.co/M23fhxm/unsplash-Eh-Tc-C9s-YXsw.png",
+        "details": "lorem hmcoooooooojkg p64444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444nj"
+    }
+]
