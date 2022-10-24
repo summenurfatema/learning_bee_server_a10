15ea2e5dbfb1506595de151bfb8bdac94de45cca
Fatema04 <shadrinmoni15@gmail.com>
2022-10-24 09:10:35 -0700

    ALL COURSE ADDED

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const cors = require('cors')
+const courses = require('./data/courses.json')
+const coursesInfo = require('./data/course-details.json')
 
 
 app.get('/', (req, res) => {
@@ -9,6 +11,26 @@ app.get('/', (req, res) => {
 })
 
 app.use(cors())
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/course-info', (req, res) => {
+    res.send(coursesInfo)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(n => n.id === id);
+    res.send(course)
+}
+)
+app.get('/course-info/:id', (req, res) => {
+    const id = req.params.id;
+    const courseInfo = coursesInfo.find(n => n.id === id);
+    res.send(courseInfo)
+}
+)
 
 
 
